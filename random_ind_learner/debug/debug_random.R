00703e3e867098335f2a1f777ad872d6a51749fb
elena <elena@elenas-mbp-2.3500.dhcp.asu.edu>
2018-09-14 17:12:34 -0700

    Debug

---
+++ random_ind_learner/debug/debug_random.R
@@ -0,0 +1,28 @@
+library(gridExtra)
+library(ggplot2)
+
+source("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/debug/learners_window_3.R")
+source("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/debug/random_learner_3_v2.R")
+
+n<-10
+s<-10
+
+n<-5
+s<-5
+
+
+sdev <- 1000000000
+r <- matrix(0, ncol=n, nrow=s)
+#assign payoffs
+for (i in 1:s){
+	r[i,] <- rexp(n, rate=1)
+	r[i,] <- round(2*(r[i,]^2))
+}
+
+l1 <- one_learner(r,sdev)
+l2 <- two_learner(r,sdev)
+l3 <- three_learner(r,sdev)
+
+
+# source("/Users/elena/Google Drive/ASU/mesoudi_model/learning_algorithms/learners_window_pay.R")
+# two dependency learners do fine
--- random_ind_learner/debug/repeats.R
+++ random_ind_learner/debug/repeats.R
@@ -7,16 +7,14 @@ library(gridExtra)
 ## PENALTY DEPENDENCIES
 ## WINDOW LEARNING
 
-# source("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/random_order_learner.R")
-source("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/random_learner_3.R")
-
+source("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/debug/random_learner_3_v2.R")
 
 # n<-20
 # s<-20
 n<-10
 s<-10
 
-repeats <- 10
+repeats <- 1000
 payoffs <- data.frame(pay=double(), algorithm=integer(), sdev=integer(), n=integer(), s= integer(), rep=integer())
 for (reps in 1:repeats){
 	r <- matrix(0, ncol=n, nrow=s)
@@ -25,12 +23,14 @@ for (reps in 1:repeats){
 		r[i,] <- rexp(n, rate=1)
 		r[i,] <- round(2*(r[i,]^2))
 	}
-	sdevs <- c(0.05, 0.1, 0.5, 1, 2, 5, 10)
+	# sdevs <- c(0.05, 0.1, 0.5, 1, 2, 5, 10)
+	sdevs <- c(100000000)
 	for (j in 1:length(sdevs)){
 		sdev <- sdevs[j]
-		l1 <- one_learner(r,sdev)
-		l2 <- two_learner(r,sdev)
-		l3 <- three_learner(r,sdev)
+		to_learn <- sample(s)
+		l1 <- one_learner(r,sdev, to_learn)
+		l2 <- two_learner(r,sdev, to_learn)
+		l3 <- three_learner(r,sdev, to_learn)
 		ps <- rbind(c(l1[[2]], l2[[2]], l3[[2]]), 1:3, rep(sdev,3),rep(n,3),rep(s,3), rep(reps,3))
 		payoffs <- rbind(payoffs, t(ps))
 	}
@@ -71,7 +71,7 @@ p <- ggplot(agg, aes(x = algorithm, y = pay, col=sdev)) +
 	ggtitle("average over 100 runs")
 	
 # png("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/pics/dep3/repeats_100runs_n10s10.png",width=400, height=400)
-png("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/pics/dep3/repeats_100runs_n20s20.png",width=400, height=400)
+# png("/Users/elena/Google Drive/ASU/mesoudi_model/random_ind_learner/pics/dep3/repeats_100runs_n20s20.png",width=400, height=400)
 p
 dev.off()
 
